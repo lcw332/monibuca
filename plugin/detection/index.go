27aa129bc3f5b095ad8f895f2f508b2ba072243c
lcw332 <lcw332@outlook.com>
2025-12-23 10:41:30 +0800

    refactor(detection): 简化图像插件配置结构
    - 移除截图模式相关配置字段
    - 移除时间间隔和关键帧间隔配置
    - 移除算法ID和置信度配置数组
    - 保留截图格式和原始图片保存选项
    - 删除未使用的time包导入

--- plugin/detection/index.go
+++ plugin/detection/index.go
@@ -1,8 +1,6 @@
 package plugin_detection
 
 import (
-	"time"
-
 	"m7s.live/v5"
 	detection "m7s.live/v5/plugin/detection/pkg"
 )
@@ -19,13 +17,8 @@ type (
 	// DetectionPlugin 图像插件
 	DetectionPlugin struct {
 		m7s.Plugin
-		SnapImgFormat  string        `json:"snapImgFormat" default:"jpg" desc:"截图文件格式(jpg/png)"`
-		SnapMode       int           `json:"snapMode" default:"0" desc:"截图模式: 0-时间间隔，1-关键帧间隔 2-HTTP请求模式（手动触发）"`
-		TimeInterval   time.Duration `json:"timeInterval" default:"1s" desc:"截图时间间隔, 仅在SnapMode为0时生效"`
-		IFrameInterval int           `json:"iframeInterval" default:"1" desc:"间隔多少帧截图, 仅在SnapMode为1时生效"`
-		AlgorithmId    []uint8       `default:"[]" desc:"算法ID"`
-		ConfThreshold  []float32     `default:"[]" desc:"全局置信度配置，与算法ID一一对应"`
-		SnapOriginal   bool          `json:"snapOriginal" default:"false" desc:"是否保存原始图片"`
+		SnapImgFormat string `json:"snapImgFormat" default:"jpg" desc:"截图文件格式(jpg/png)"`
+		SnapOriginal  bool   `json:"snapOriginal" default:"false" desc:"是否保存原始图片"`
 		// 对象
 		AlgorithmAPI detection.AlgorithmAPI `json:"algorithmApi" default:"{}" desc:"算法API配置"`
 		Oss          detection.Oss          `json:"oss" default:"{}" desc:"对象存储公共配置"`
